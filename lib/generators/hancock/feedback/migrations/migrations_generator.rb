1cc4d62d6f6ff7493339c5860d0832339df85d17
Alexander Kiseliev <i43ack@gmail.com>
2016-11-23 10:37:04 +0300

    generators upd; dependencies

--- lib/generators/hancock/feedback/migrations/migrations_generator.rb
+++ lib/generators/hancock/feedback/migrations/migrations_generator.rb
@@ -2,12 +2,12 @@ require 'rails/generators'
 require 'rails/generators/active_record'
 
 module Hancock::Feedback
-  class MigrationGenerator < Rails::Generators::Base
+  class MigrationsGenerator < Rails::Generators::Base
     include ActiveRecord::Generators::Migration
     source_root File.expand_path('../templates', __FILE__)
 
     desc 'Hancock Feedback migration generator'
-    def install
+    def migrations
       if Hancock::Feedback.active_record?
         %w(contact_messages).each do |table_name|
           migration_template "migration_#{table_name}.rb", "db/migrate/hancock_feedback_create_#{table_name}.rb"
