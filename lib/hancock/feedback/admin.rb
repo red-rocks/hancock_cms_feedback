d680f0feabec17e55d5f3358a7e8610669627c5c
Alexander Kiseliev <i43ack@gmail.com>
2016-12-24 14:52:22 +0300

    rails_admin config upd

--- lib/hancock/feedback/admin.rb
+++ lib/hancock/feedback/admin.rb
@@ -1,4 +1,6 @@
 module Hancock::Feedback
   module Admin
+    class << self
+    end
   end
 end
--- lib/hancock/feedback/admin/contact_message.rb
+++ lib/hancock/feedback/admin/contact_message.rb
@@ -1,9 +1,16 @@
 module Hancock::Feedback
   module Admin
     module ContactMessage
-      def self.config(fields = {})
+      def self.config(nav_label = nil, fields = {})
+        if nav_label.is_a?(Hash)
+          nav_label, fields = nav_label[:nav_label], nav_label
+        elsif nav_label.is_a?(Array)
+          nav_label, fields = nil, nav_label
+        end
+
         Proc.new {
-          navigation_label I18n.t('hancock.feedback')
+          navigation_label(!nav_label.blank? ? nav_label : I18n.t('hancock.feedback'))
+          
           field :c_at do
             read_only true
           end
