3dcb9b752f6799a42f108b6878db9132608b3c33
Alexander Kiseliev <i43ack@gmail.com>
2017-02-26 15:17:16 +0300

    engien intializer upd

--- lib/hancock/feedback/engine.rb
+++ lib/hancock/feedback/engine.rb
@@ -6,31 +6,31 @@ module Hancock::Feedback
     #   require File.expand_path('../tasks', __FILE__)
     # end
 
-    initializer "hancock_cms_feedback.email_defaults" do
-      # Write default email settings to DB so they can be changed.
-
-      #temp
-      begin
-        if Settings and Settings.table_exists?
-          Settings.default_email_from(default: 'noreply@redrocks.pro')
-          Settings.form_email(default: 'admin@redrocks.pro')
-          Settings.email_topic(default: 'с сайта')
-        end
-      rescue
-      end
-    end
-
-    # config.after_initialize do
+    # initializer "hancock_cms_feedback.email_defaults" do
     #   # Write default email settings to DB so they can be changed.
+    #
+    #   #temp
     #   begin
     #     if Settings and Settings.table_exists?
-    #       Settings.default_email_from(default: 'noreply@site.domain')
-    #       Settings.form_email(default: 'admin@site.domain')
+    #       Settings.default_email_from(default: 'noreply@redrocks.pro')
+    #       Settings.form_email(default: 'admin@redrocks.pro')
     #       Settings.email_topic(default: 'с сайта')
     #     end
     #   rescue
     #   end
     # end
+
+    config.after_initialize do
+      # Write default email settings to DB so they can be changed.
+      begin
+        if Settings and Settings.table_exists?
+          Settings.default_email_from(default: 'noreply@site.domain') unless RailsAdminSettings::Setting.ns("main").where(key: "default_email_from").exists?
+          Settings.form_email(default: 'admin@site.domain') unless RailsAdminSettings::Setting.ns("main").where(key: "form_email").exists?
+          Settings.email_topic(default: 'с сайта') unless RailsAdminSettings::Setting.ns("main").where(key: "email_topic").exists?
+        end
+      rescue
+      end
+    end
 
   end
 end
