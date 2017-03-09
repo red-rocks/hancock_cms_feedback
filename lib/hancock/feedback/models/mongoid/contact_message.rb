a769bcb7e7eddaa2bceeabb070e8935c44c01c64
Alexander Kiseliev <i43ack@gmail.com>
2017-03-09 14:10:15 +0300

    patch for belongs_to updater

--- lib/hancock/feedback/models/mongoid/contact_message.rb
+++ lib/hancock/feedback/models/mongoid/contact_message.rb
@@ -8,7 +8,27 @@ module Hancock::Feedback
           field :email, type: String
           field :phone, type: String
           field :content, type: String
+
+          if Hancock.rails5?
+            if relations.has_key?("updater")
+              belongs_to :updater, class_name: ::Mongoid::History.modifier_class_name, optional: true, validate: false
+              _validators.delete(:updater)
+              _validate_callbacks.each do |callback|
+                if callback.raw_filter.respond_to?(:attributes) and callback.raw_filter.attributes.include?(:updater)
+                  _validate_callbacks.delete(callback)
+                end
+              end
+
+            end
+          end
         end
+
+        class_methods do
+          def track_history?
+            false
+          end
+        end
+
       end
     end
   end
